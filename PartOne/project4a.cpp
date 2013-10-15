13f9c14ce2c9febf16ca47160680920c79c0cb98
hue <gth626u@gmail.com>
2013-10-15 00:26:55 -0400

    Implemented and removed more pseudocode.

--- PartOne/project4a.cpp
+++ PartOne/project4a.cpp
@@ -15,180 +15,136 @@
 /* the buffer */
 buffer_item buffer[BUFFER_SIZE];
 
-sem_t empty;
-sem_t full;
-
-pthread_mutex_t bufferMutex = PTHREAD_MUTEX_INITIALIZER; //mutex lock
+pthread_mutex_t bufferMutex = PTHREAD_MUTEX_INITIALIZER; // mutex lock
+sem_t empty; // semaphore consumer increments producer decrements
+sem_t full; // semaphore producer increments consumer decrements
 
 void *createProducer(void *param); /* threads call this function */
 void *createConsumer(void *param); /* threads call this function */
 
 int main(int argc, char *argv[]) {
-	int sleep, numProducer, numConsumer;
-
 	if (argc != 4) {
 		fprintf(stderr, "usage: a.out <sleep> <numProducer> <numConsumer>");
 		return -1;
 	}
 
-	sleep = atoi(argv[1]);
-	numProducer = atoi(argv[2]);
-	numConsumer = atoi(argv[3]);
-	pthread_t tid[numConsumer + numProducer]; /*create array of threads using the sum of the arguments*/
-	for (int i = 0; i < numConsumer; i++)
-	{
+	int sleep = atoi(argv[1]);
+	int numProducer = atoi(argv[2]);
+	int numConsumer = atoi(argv[3]);
+
+	pthread_t tid[numConsumer + numProducer]; // create array of threads using the sum of the arguments
+
+	for (int i = 0; i < numConsumer; i++) {
 		pthread_create(&tid[i], NULL, createConsumer, 0);
 	}
-	for (int j = 0; j < numProducer; j++)
-	{
+	for (int j = 0; j < numProducer; j++) {
 		pthread_create(&tid[j], NULL, createProducer, 0);
 	}
 
 	/* wait for the thread to exit */
-	for (int i = 0; i < numConsumer; i++)
-	{
+	for (int i = 0; i < numConsumer; i++) {
 		pthread_join(tid[i], NULL);
 	}
-	for (int j = 0; j < numConsumer; j++)
-	{
+	for (int j = 0; j < numConsumer; j++) {
 		pthread_join(tid[j], NULL);
 	}
 
-	pthread_mutex_init(&bufferMutex,NULL);
+	pthread_mutex_init(&bufferMutex, NULL);
 	sem_init(&empty, 0, BUFFER_SIZE);
 	sem_init(&full, 0, 0);
 
 	/* 2. Initialize buffer */
-	/* 3. Create producer thread(s) */
-	/* 4. Create consumer thread(s) */
 	/* 5. Sleep */
 	/* 6. Exit */
 }
 
-void *createProducer(void *param)
-{
+void *createProducer(void *param) {
+//	do {
+//		//. . .
+//		/* produce an item in next produced */
+//		//. . .
+//		sem_wait(&empty);
+//		pthread_mutex_lock(&bufferMutex);
+//		//. . .
+//		/* add next produced to the buffer */
+//		//. . .
+//		pthread_mutex_unlock(&bufferMutex);
+//		sem_post(&full);
+//	} while (true);
+
+	/*
+	 * Windows API Implementation
+	 */
+
+//	buffer_item item;
+//
+//	while (true) {
+//		/* sleep for a random period of time */
+//		sleep(...);
+//		/* generate a random number */
+//		item = rand();
+//		if (insert_item(item))
+//			printf("report error condition");
+//		else
+//			printf("producer produced %d\n", item);
+//	}
 	pthread_exit(NULL);
 }
 
+void *createConsumer(void *param) {
+//	do {
+//		sem_wait(&full);
+//		pthread_mutex_lock(&bufferMutex);
+//		//. . .
+//		/* remove an item from buffer to next consumed */
+//		//. . .
+//		pthread_mutex_unlock(&bufferMutex);
+//		sem_post(&empty);
+//		//. . .
+//		/* consume the item in next consumed */
+//		. . .
+//	} while (true);
 
-void *createConsumer(void *param)
-{
+	/*
+	 * Windows API Implementation
+	 */
+
+	//	buffer_item item;
+	//
+	//	while (true) {
+	//		/* sleep for a random period of time */
+	//		sleep(...);
+	//		if (remove_item(item))
+	//			printf("report error condition");
+	//		else
+	//			printf("consumer consumed %d\n", item);
+	//	}
 	pthread_exit(NULL);
 }
 
 int insert_item(buffer_item item) {
 	/* insert item into buffer
-	return 0 if successful, otherwise
-	return -1 indicating an error condition */
-
-	//do {
-	//	//. . .
-	//	/* produce an item in next produced */
-	//	//. . .
-	//	sem_wait(&empty);
-	//	sem_wait(&mutex);
-	//	//. . .
-	//	/* add next produced to the buffer */
-	//	//. . .
-	//	sem_post(&mutex);
-	//	sem_post(&full);
-	//} while (true);
+	 return 0 if successful, otherwise
+	 return -1 indicating an error condition */
 }
 
 int remove_item(buffer_item item) {
 	/* remove an object from buffer
-	placing it in item
-	return 0 if successful, otherwise
-	return -1 indicating an error condition */
-
-	//do {
-	//	sem_wait(&full);
-	//	sem_wait(&mutex);
-	//	//. . .
-	//	/* remove an item from buffer to next consumed */
-	//	//. . .
-	//	sem_post(&mutex);
-	//	sem_post(&empty);
-	//	//. . .
-	//	/* consume the item in next consumed */
-	//	. . .
-	//} while (true);
+	 placing it in item
+	 return 0 if successful, otherwise
+	 return -1 indicating an error condition */
 }
 
 /*
  * Code from Section 4.4.1 - Pthreads
  */
 
-//int sum; /* this data is shared by the thread(s) */
-//semaphore empty = n; //semaphore consumer increments producer decrements
-//semaphore full = 0 //semaphore producer increments consumer decrements
-//void *runner(void *param); /* threads call this function */
-//
 //int main(int argc, char *argv[])
 //{
-//	pthread_t tid; /* the thread identifier */
 //	pthread_attr_t attr; /* set of thread attributes */
 //
 //	/* get the default attributes */
 //	pthread_attr_init(&attr);
 //	/* create the thread */
 //	pthread_create(&tid,&attr,runner,argv[1]);
-//	/* wait for the thread to exit */
-//	pthread_join(tid,NULL);
-//
-//	printf("sum = %d\n",sum);
 //}
-//
-///* The thread will begin control in this function */
-//void *runner(void *param)
-//{
-//	int i, upper = atoi((char*)param);
-//	sum = 0;
-//
-//	for (i = 1; i <= upper; i++)
-//		sum += i;
-//
-//	pthread_exit(0);
-//}
-
-/*
- * Joining multiple threads.
- */
-
-//#define NUM_THREADS 10
-//
-///* an array of threads to be joined upon */
-//pthread_t workers[NUM THREADS];
-//
-//for (int i = 0; i < NUM_THREADS; i++)
-//	pthread_join(workers[i], NULL);
-
-/*
- * Code from Section 5.9.4 - Pthreads Synchronization
- */
-
-/*
- * Protecting a critical section with mutex locks.
- */
-
-///* acquire the mutex lock */
-//pthread_mutex_lock(&mutex);
-//
-///* critical section */
-//
-///* release the mutex lock */
-//pthread_mutex_unlock(&mutex);
-
-/*
- * Protecting a critical section using a semaphore.
- */
-
-//* acquire the semaphore */
-//sem_wait(&sem);
-//
-///* critical section */
-//
-///* release the semaphore */
-//sem post(&sem);
-
-//sem_post(&sem);
