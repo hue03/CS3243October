babddde076e1c0f9f79e557b9554b25513c8a79a
hue <gth626u@gmail.com>
2013-10-14 22:52:08 -0400

    Implemented some pseudocode.

--- PartOne/project4a.cpp
+++ PartOne/project4a.cpp
@@ -5,6 +5,62 @@
 // Date: 10/14/2013
 // File: project4a.cpp
 
+#include "buffer.h"
+#include <semaphore.h>
+#include <stdio.h>
+#include <iostream>
+#include <pthread.h>
+#include <cstdlib>
+
+/* the buffer */
+buffer_item buffer[BUFFER_SIZE];
+
+int n;
+sem_t mutex /*= 1*/;
+sem_t empty /*= n*/;
+sem_t full /*= 0*/;
+
+int insert_item(buffer_item item) {
+	/* insert item into buffer
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
+
+int remove_item(buffer_item item) {
+	/* remove an object from buffer
+	placing it in item
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
+}
+
+//1. How long to sleep before terminating
+//2. The number of producer threads
+//3. The number of consumer threads
+
+int main(int argc, char *argv[]) {
+	int sleep, numProducer, numConsumer;
+
+	if (argc != 4) {
+		std::cout << "You entered the wrong number of command-line arguments."
+				<< "How long should main sleep before terminating?\n";
+		std::cin >> sleep;
+		std::cout << "How many producer threads are there?\n";
+		std::cin >> numProducer;
+		std::cout << "How many consumer threads are there?\n";
+		std::cin >> numConsumer;
+	} else {
+		sleep = atoi(argv[1]);
+		numProducer = atoi(argv[2]);
+		numConsumer = atoi(argv[3]);
+	}
+	/* 1. Get command line arguments argv[1],argv[2],argv[3] */
+	/* 2. Initialize buffer */
+	/* 3. Create producer thread(s) */
+	/* 4. Create consumer thread(s) */
+	/* 5. Sleep */
+	/* 6. Exit */
+}
+
 /*
  * Code from Section 5.7.1 - The Bounded Buffer Problem
  */
@@ -13,26 +69,21 @@
  * Data structures shared by the producer and consumer processes.
  */
 
-//int n;
-//semaphore mutex = 1;
-//semaphore empty = n;
-//semaphore full = 0
-
 /*
  * The structure of the producer process.
  */
 
 //do {
-//	. . .
+//	//. . .
 //	/* produce an item in next produced */
-//	. . .
-//	wait(empty);
-//	wait(mutex);
-//	. . .
+//	//. . .
+//	sem_wait(&empty);
+//	sem_wait(&mutex);
+//	//. . .
 //	/* add next produced to the buffer */
-//	. . .
-//	signal(mutex);
-//	signal(full);
+//	//. . .
+//	sem_post(&mutex);
+//	sem_post(&full);
 //} while (true);
 
 /*
@@ -40,80 +91,29 @@
  */
 
 //do {
-//	wait(full);
-//	wait(mutex);
-//	. . .
+//	sem_wait(&full);
+//	sem_wait(&mutex);
+//	//. . .
 //	/* remove an item from buffer to next consumed */
-//	. . .
-//	signal(mutex);
-//	signal(empty);
-//	. . .
+//	//. . .
+//	sem_post(&mutex);
+//	sem_post(&empty);
+//	//. . .
 //	/* consume the item in next consumed */
 //	. . .
 //} while (true);
 
 /*
- * Code from Programming Project description
- */
-
-/*
- * Outline of buffer operations.
- */
-
-//#include "buffer.h"
-//
-///* the buffer */
-//buffer item buffer[BUFFER SIZE];
-//
-//int insert item(buffer item item) {
-//	/* insert item into buffer
-//	return 0 if successful, otherwise
-//	return -1 indicating an error condition */
-//}
-//
-//int remove item(buffer item) {
-//	/* remove an object from buffer
-//	placing it in item
-//	return 0 if successful, otherwise
-//	return -1 indicating an error condition */
-//}
-
-/*
- * The buffer header file
- */
-
-///* buffer.h */
-//typedef int buffer item;
-//#define BUFFER SIZE 5
-
-/*
- * The main() function
- */
-
-//#include "buffer.h"
-//int main(int argc, char *argv[]) {
-//	/* 1. Get command line arguments argv[1],argv[2],argv[3] */
-//	/* 2. Initialize buffer */
-//	/* 3. Create producer thread(s) */
-//	/* 4. Create consumer thread(s) */
-//	/* 5. Sleep */
-//	/* 6. Exit */
-//}
-
-/*
  * Code from Section 4.4.1 - Pthreads
  */
 
-//#include <pthread.h>
-//#include <stdio.h>
-//
-//int sum; /* this data is shared by the thread(s) */
-//void *runner(void *param); /* threads call this function */
-//
+int sum; /* this data is shared by the thread(s) */
+void *runner(void *param); /* threads call this function */
+
 //int main(int argc, char *argv[])
 //{
-//	pthread t tid; /* the thread identifier */
-//	pthread attr t attr; /* set of thread attributes */
+//	pthread_t tid; /* the thread identifier */
+//	pthread_attr_t attr; /* set of thread attributes */
 //
 //	if (argc != 2) {
 //		fprintf(stderr,"usage: a.out <integer value>\n");
@@ -125,38 +125,38 @@
 //	}
 //
 //	/* get the default attributes */
-//	pthread attr init(&attr);
+//	pthread_attr_init(&attr);
 //	/* create the thread */
-//	pthread create(&tid,&attr,runner,argv[1]);
+//	pthread_create(&tid,&attr,runner,argv[1]);
 //	/* wait for the thread to exit */
-//	pthread join(tid,NULL);
+//	pthread_join(tid,NULL);
 //
 //	printf("sum = %d\n",sum);
 //}
-//
-///* The thread will begin control in this function */
-//void *runner(void *param)
-//{
-//	int i, upper = atoi(param);
-//	sum = 0;
-//
-//	for (i = 1; i <= upper; i++)
-//		sum += i;
-//
-//	pthread exit(0);
-//}
+
+/* The thread will begin control in this function */
+void *runner(void *param)
+{
+	int i, upper = atoi((char*)param);
+	sum = 0;
+
+	for (i = 1; i <= upper; i++)
+		sum += i;
+
+	pthread_exit(0);
+}
 
 /*
- * pthread.h header file
+ * Joining multiple threads.
  */
 
-//#define NUM THREADS 10
+//#define NUM_THREADS 10
 //
 ///* an array of threads to be joined upon */
-//pthread t workers[NUM THREADS];
+//pthread_t workers[NUM THREADS];
 //
-//for (int i = 0; i < NUM THREADS; i++)
-//	pthread join(workers[i], NULL);
+//for (int i = 0; i < NUM_THREADS; i++)
+//	pthread_join(workers[i], NULL);
 
 /*
  * Code from Section 5.9.4 - Pthreads Synchronization
@@ -168,41 +168,41 @@
 
 //#include <pthread.h>
 //
-//pthread mutex t mutex;
+//pthread_mutex_t mutex;
 //
 ///* create the mutex lock */
-//pthread mutex init(&mutex,NULL);
+//pthread_mutex_init(&mutex,NULL);
 
 /*
  * Protecting a critical section with mutex locks.
  */
 
 ///* acquire the mutex lock */
-//pthread mutex lock(&mutex);
+//pthread_mutex_lock(&mutex);
 //
 ///* critical section */
 //
 ///* release the mutex lock */
-//pthread mutex unlock(&mutex);
+//pthread_mutex_unlock(&mutex);
 
 /*
  * Creating and initializing an unnamed semaphore.
  */
 
 //#include <semaphore.h>
-//sem t sem;
+//sem_t sem;
 //
 ///* Create the semaphore and initialize it to 1 */
-//sem init(&sem, 0, 1);
+//sem_init(&sem, 0, 1);
 
 /*
  * Protecting a critical section using a semaphore.
  */
 
 //* acquire the semaphore */
-//sem wait(&sem);
+//sem_wait(&sem);
 //
 ///* critical section */
 //
 ///* release the semaphore */
-//sem post(&sem);
+//sem_post(&sem);
