670b46c6f9eb31a394e499b875479b0e10174cdf
hue <gth626u@gmail.com>
2013-10-15 07:52:32 -0400

    Major overhaul.

--- PartOne/buffer.h
+++ PartOne/buffer.h
@@ -9,12 +9,16 @@
 #define BUFFER_H_
 
 typedef int buffer_item;
-#define BUFFER_SIZE 5
+#define BUFFER_SIZE 6
 
 struct Buffer {
 	size_t start;
 	size_t end;
+	size_t count;
 	buffer_item buffer[BUFFER_SIZE];
+	pthread_mutex_t mutex;
+	sem_t empty;
+	sem_t full;
 
 	buffer_item& operator[](int i) {
 		buffer_item& value = buffer[i];
@@ -24,17 +28,44 @@ struct Buffer {
 	Buffer() {
 		start = 0;
 		end = 0;
+		count = 0;
+
+		/* create the mutex lock */
+		pthread_mutex_init(&mutex, NULL);
+
+		/* Create the semaphore and initialize it to BUFFER_SIZE */
+		sem_init(&empty, 0, BUFFER_SIZE);
+
+		/* Create the semaphore and initialize it to 0 */
+		sem_init(&full, 0, 0);
 	}
 
+	/* insert item into buffer
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
 	int insert_item(buffer_item it) {
+		if (BUFFER_SIZE == count) {
+			return -1;
+		}
+
 		buffer[end] = it;
 		end = (end + 1) % BUFFER_SIZE;
+		++count;
 		return 0;
 	}
 
+	/* remove an object from buffer
+	placing it in item
+	return 0 if successful, otherwise
+	return -1 indicating an error condition */
 	int remove_item(buffer_item *it) {
+		if (0 == count) {
+			return -1;
+		}
+
 		*it = buffer[start];
 		start = (start + 1) % BUFFER_SIZE;
+		--count;
 		return 0;
 	}
 };
--- PartOne/project4a.cpp
+++ PartOne/project4a.cpp
@@ -7,378 +7,140 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
-#include <math.h>
 #include <unistd.h>
 #include "buffer.h"
+
+#define SEED time(NULL)
+#define P_RAND_SLEEP 10
+#define C_RAND_SLEEP 10
 
 using namespace std;
 
 /* the buffer */
 Buffer buffer;
-//buffer_item buffer[BUFFER_SIZE];
-size_t a;
-buffer_item item;
-pthread_mutex_t bufferMutex; // mutex lock
-sem_t empty; // semaphore consumer increments producer decrements
-sem_t full; // semaphore producer increments consumer decrements
-uint timer; //sleep timer
-int seed; //use to help seed random number
-buffer_item *element; //pointer to the location of an element
-int index; //use for selecting which element to remove
 
-void *createProducer(void *param); /* threads call this function */
-void *createConsumer(void *param); /* threads call this function */
-int insert_item(buffer_item);
-int remove_item(buffer_item *);
-void printStuff(int);
+void *producer(void*); /* threads call this function */
+void *consumer(void*); /* threads call this function */
 
-void insert();
+string Color(size_t);
 
 int main(int argc, char *argv[]) {
+	/* 1. Get command line arguments argv[1],argv[2],argv[3] */
+
 	if (argc != 4) {
-//		fprintf(stderr, "usage: a.out <sleepTime> <numProducer> <numConsumer>");
-		cout << "!!!Invalid Arguments!!!\n_________________________" << endl;
-		fprintf(stderr, "Format your arguments as follow: [output file] <integer # for sleep> <integer # for amount of producer threads> <integer # for amount of consumer threads>");
+		cout << "!!!Invalid Arguments!!!\n_______________________" << endl;
+		fprintf(stderr,
+				"Format your arguments as follow:  [output file] <integer # for sleep> <integer # for amount of producer threads> <integer # for amount of consumer threads>");
 		return -1;
 	}
-	//initialize index to 0 and have element point to the memory address of the element at that index
-	index = 0;
-	element = &(buffer[index]);
-	
-	seed = 2;
 
-	int sleepTime = atoi(argv[1]);
-
-	timer = atoi(argv[1]);
-	int numProducer = atoi(argv[2]);
-	int numConsumer = atoi(argv[3]);
-
-	buffer = Buffer();
-
-	pthread_mutex_init(&bufferMutex, NULL);
-	sem_init(&empty, 0, BUFFER_SIZE);
-	sem_init(&full, 0, 0);
-
-	pthread_t consumerThread[numConsumer]; /*create array of consumer threads*/
-	pthread_t producerThread[numProducer]; /*create array of producer threads*/
-	
-	//create threads
-	for (int i = 0; i < numConsumer; i++)
-	{
-		pthread_create(&consumerThread[i], NULL, createConsumer, (void*)((intptr_t) i));
-	}
-	
-	for (int j = 0; j < numProducer; j++)
-	{
-		pthread_create(&producerThread[j], NULL, createProducer, (void*)((intptr_t) j));
-	}
-
-
-//	int n;
-//
-//	do
-//	{
-//		n = -1;
-//
-//		cout << "Enter how many random numbers into the buffer?\n";
-//		cin >> n;
-//
-//		while (n < 0) {
-//			cin >> n;
-//		}
-//
-//		n = min(n, BUFFER_SIZE);
-//
-//		for (int i = 0; i < n; ++i) {
-//			insert();
-//		}
-//	} while (n > 0);
-
-	sleep(sleepTime);
-
-	/* wait for the thread to exit */
-	for (int i = 0; i < numProducer; i++)
-	{
-		pthread_join(producerThread[i], NULL);
-	}
-	
-	for (int j = 0; j < numConsumer; j++)
-	{
-		pthread_join(consumerThread[j], NULL);
-	}
-
-	// TODO These aren't in the right place.
-	//pthread_mutex_init(&bufferMutex, NULL);
-	//sem_init(&empty, 0, BUFFER_SIZE);
-	//sem_init(&full, 0, 0);
+	uint sleepTime = atoi(argv[1]);
+	uint numProducer = atoi(argv[2]);
+	uint numConsumer = atoi(argv[3]);
 
 	/* 2. Initialize buffer */
+	buffer = Buffer();
+
+	srand(SEED);
+
+	/* 3. Create producer thread(s) */
+	for (uint i = 0; i < numProducer; ++i) {
+		pthread_t thread;
+		int n = i;
+
+		pthread_create(&thread, NULL, producer, &n);
+	}
+
+	/* 4. Create consumer thread(s) */
+	for (uint i = 0; i < numConsumer; ++i) {
+		pthread_t thread;
+		int n = i;
+
+		pthread_create(&thread, NULL, consumer, &n);
+	}
+
 	/* 5. Sleep */
+	sleep(sleepTime);
+
 	/* 6. Exit */
+	return 0;
 }
 
-void insert() {
-	sleep(1);
+void *producer(void *param) {
+	int i = *(int*) param + 1;
 
-	buffer_item item = rand();
+	for (;;) {
+		/* sleep for a random period of time */
+		sleep(rand() % P_RAND_SLEEP + 1);
 
-	sem_wait(&empty);
+		/* generate a random number */
+		buffer_item item = rand();
 
-	pthread_mutex_lock(&bufferMutex);
+		sem_wait(&buffer.empty); /* acquire the semaphore */
+		pthread_mutex_lock(&buffer.mutex); /* acquire the mutex lock */
 
-	if (buffer.insert_item(item)) {
-		printf("report error condition");
-	} else {
-		switch(buffer.end) {
-		case 0:
-//			"\033[1;31mbold red text\033[0m\n"
-//			\[\033[0;32m\]
-			cout << "\033[32m";
-			cout << "blah";
-			break;
-		case 1:
-			cout << "\033[37m";
-			break;
-		case 2:
-			cout << "\033[36m";
-			system("Color 3C");
-			break;
-		case 3:
-			cout << "\033[33m";
-			system("Color 4D");
-			break;
-		case 4:
-			cout << "\033[35m";
-			system("Color 5E");
-			break;
+		if (buffer.insert_item(item)) {
+			printf("report error condition");
+		} else {
+			cout << "Producer " << i << " (ID: " << pthread_self()
+					<< ") produced random number " << Color(buffer.end) << item
+					<< "\033[0m\n";
 		}
-		printf("Inserted %d\n", item);
+
+		pthread_mutex_unlock(&buffer.mutex); /* release the mutex lock */
+		sem_post(&buffer.full); /* release the semaphore */
 	}
-
-	pthread_mutex_unlock(&bufferMutex);
-	sem_post(&full);
-}
-
-void *createProducer(void *param) {
-	do 
-	{
-		//printStuff(pthread_self());
-		sleep(timer);
-		sem_wait(&empty);
-		pthread_mutex_lock(&bufferMutex);
-		
-		cout << "Producer is no longer a lazy bum and is doing something productive." << endl;
-		srand(pthread_self() * log(seed)); //log op on seed will generate new random numbers without having it become too large over time
-		seed++;
-		item = rand();
-		//int id = *((int*)(&param));
-		//cout << "Thread: " << id <<" Is Inserting " << item << endl;
-		cout << "Inserting " << item << endl;
-		if (insert_item(item) == 0)
-		{
-			cout << "Insertion Successful!" << endl;
-			cout << "_____________________" << endl;
-		}
-		else
-		{
-			cout << "Insertion Unsuccessful." << endl;
-		}
-		
-		pthread_mutex_unlock(&bufferMutex);
-		sem_post(&full);
-	} while(true);
-
-	/*for (int i = 0; i < BUFFER_SIZE; i++)
-	{
-		cout << buffer[i] << endl;
-	}*/	
-//	do {
-//		//. . .
-//		/* produce an item in next produced */
-//		//. . .
-//		sem_wait(&empty);
-//		pthread_mutex_lock(&bufferMutex);
-//		//. . .
-//		/* add next produced to the buffer */
-//		//. . .
-//		pthread_mutex_unlock(&bufferMutex);
-//		sem_post(&full);
-//	} while (true);
-
-	/*
-	 * Windows API Implementation
-	 */
-
-//	buffer_item item;
-//
-//	while (true) {
-//		/* sleep for a random period of time */
-//		sleep(...);
-//		/* generate a random number */
-//		item = rand();
-//		if (insert_item(item))
-//			printf("report error condition");
-//		else
-//			printf("producer produced %d\n", item);
-//	}
-
-	//cout << pthread_self() << endl;
 
 	pthread_exit(NULL);
 }
 
-void *createConsumer(void *param) {
-	do 
-	{
-		//printStuff(pthread_self());
-		sleep(timer);
-		sem_wait(&full);
-		pthread_mutex_lock(&bufferMutex);
-		
-		cout << "Consumer is being fat." << endl;
-		//srand(pthread_self() * log(seed));
-		//seed++;
-		//item = rand();
-		//int id = *((int*)(&param));
-		//cout << "Thread: " << id <<" Is Inserting " << item << endl;
-		if (buffer[index] != 0)
-		{
-			cout << "Removing an item from buffer: " << buffer[index] << endl;
-			if (remove_item(element) == 0)
-			{
-				cout << "Removal Successful!" << endl;
-				cout << "_____________________" << endl;
-			}
-			else
-			{
-				cout << "Removal Unsuccessful." << endl;
-			}
+void *consumer(void *param) {
+	int i = *(int*) param + 1;
+
+	for (;;) {
+		/* sleep for a random period of time */
+		sleep(rand() % C_RAND_SLEEP + 1);
+
+		buffer_item item;
+
+		sem_wait(&buffer.full); /* acquire the semaphore */
+		pthread_mutex_lock(&buffer.mutex); /* acquire the mutex lock */
+
+		/* critical section */
+		if (buffer.remove_item(&item)) {
+			printf("report error condition");
+		} else {
+			cout << "Consumer " << i << " (ID: " << pthread_self()
+					<< ") consumed random number " << Color(buffer.start)
+					<< item << "\033[0m\n";
 		}
-		else
-		{
-			cout << "Element at " << index << " is empty." << endl;
-			cout << "_____________________" << endl;
-		}
-		index = (index + 1) % BUFFER_SIZE;
-		element = &(buffer[index]);
-		
-		pthread_mutex_unlock(&bufferMutex);
-		sem_post(&empty);
-	} while(true);
 
-	/*for (int i = 0; i < BUFFER_SIZE; i++)
-	{
-		cout << buffer[i] << endl;
-	}*/
-//	do {
-//		sem_wait(&full);
-//		pthread_mutex_lock(&bufferMutex);
-//		//. . .
-//		/* remove an item from buffer to next consumed */
-//		//. . .
-//		pthread_mutex_unlock(&bufferMutex);
-//		sem_post(&empty);
-//		//. . .
-//		/* consume the item in next consumed */
-//		. . .
-//	} while (true);
-
-//	for (;;) {
-//		sleep(1);
-//		buffer_item item;
-//		int size;
-//
-//		do {
-//			sem_getvalue(&full, &size);
-//		} while (BUFFER_SIZE == size);
-//
-//		sem_wait(&full);
-//
-//		pthread_mutex_lock(&bufferMutex);
-//
-//		if (buffer.remove_item(&item)) {
-//			printf("report error condition");
-//		else
-//			printf("Consumer %u consumed %d\n", pthread_self(), item);
-//		}
-//
-//		pthread_mutex_unlock(&bufferMutex);
-//		sem_post(&empty);
-//	}
-
-	/*
-	 * Windows API Implementation
-	 */
-
-//	buffer_item item;
-//
-//	while (true) {
-//		/* sleep for a random period of time */
-//		sleep(...);
-//		if (remove_item(item))
-//			printf("report error condition");
-//		else
-//			printf("consumer consumed %d\n", item);
-//	}
-
-	//cout << pthread_self() << endl;
+		pthread_mutex_unlock(&buffer.mutex); /* release the mutex lock */
+		sem_post(&buffer.empty); /* release the semaphore */
+	}
 
 	pthread_exit(NULL);
 }
 
-/* insert item into buffer.
-return 0 if successful, otherwise
- return -1 indicating an error condition.*/
-int insert_item(buffer_item it) {
-	for (int i = 0; i < BUFFER_SIZE; i++)
-	{
-		if (buffer[i] == 0)
-		{
-			buffer[i] = it;
-			return 0;
-		}
+string Color(size_t n) {
+	string result = "";
+
+	switch (n) {
+	case 0:
+		return result + "\033[31m";
+	case 1:
+		return result + "\033[32m";
+	case 2:
+		return result + "\033[33m";
+	case 3:
+		return result + "\033[34m";
+	case 4:
+		return result + "\033[35m";
+	case 5:
+		return result + "\033[36m";
 	}
-	return -1;
-}
 
-/*Remove an object from buffer
-placing it in item.
-return 0 if successful, otherwise
-return -1 indicating an error condition.*/
-int remove_item(buffer_item *it) {
-	int temp = *it;
-	for (int i = 0; i < BUFFER_SIZE; i++)
-	{
-		if (buffer[i] == temp)
-		{
-			//buffer[i] = 0;
-			//it = 0;
-			*it = 0;
-			return 0;
-		}	
-	}
-	return -1;
+	return "\033[0m";
 }
-
-void printStuff(int tid)
-{
-	pthread_mutex_lock(&bufferMutex);
-	cout <<"Thread: " << tid << endl;
-	pthread_mutex_unlock(&bufferMutex);
-}
-/*
- * Code from Section 4.4.1 - Pthreads
- */
-
-//int main(int argc, char *argv[])
-//{
-//	pthread_attr_t attr; /* set of thread attributes */
-//
-//	/* get the default attributes */
-//	pthread_attr_init(&attr);
-//	/* create the thread */
-//	pthread_create(&tid,&attr,runner,argv[1]);
-//}
