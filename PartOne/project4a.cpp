2209f375ab3f49398d9b8a58711a5e0f652c5373
Steven Ng <stevenng308@gmail.com>
2013-10-15 12:07:42 -0400

    Producer and Consumer code is somewhat work. Can insert and remove items in circular order but there seems to be a deadlock.

--- PartOne/project4a.cpp
+++ PartOne/project4a.cpp
@@ -22,6 +22,8 @@ pthread_mutex_t bufferMutex; // mutex lock
 sem_t empty; // semaphore consumer increments producer decrements
 sem_t full; // semaphore producer increments consumer decrements
 int seed; //use to help seed random number
+buffer_item *element; //pointer to the location of an element
+int index; //use for selecting which element to remove
 
 void *createProducer(void *param); /* threads call this function */
 void *createConsumer(void *param); /* threads call this function */
@@ -35,6 +37,10 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Format your arguments as follow: [output file] <integer # for sleep> <integer # for amount of producer threads> <integer # for amount of consumer threads>");
 		return -1;
 	}
+	//initialize index to 0 and have element point to the memory address of the element at that index
+	index = 0;
+	element = &(buffer[index]);
+	
 	seed = 2;
 	int sleep = atoi(argv[1]);
 	int numProducer = atoi(argv[2]);
@@ -44,26 +50,29 @@ int main(int argc, char *argv[]) {
 	sem_init(&empty, 0, BUFFER_SIZE);
 	sem_init(&full, 0, 0);
 
-	pthread_t consumerThread[numConsumer]; /*create array of threads using the sum of the arguments*/
-	pthread_t producerThread[ numProducer]; /*create array of threads using the sum of the arguments*/
-
+	pthread_t consumerThread[numConsumer]; /*create array of consumer threads*/
+	pthread_t producerThread[numProducer]; /*create array of producer threads*/
+	
+	//create threads
 	for (int i = 0; i < numConsumer; i++)
 	{
 		pthread_create(&consumerThread[i], NULL, createConsumer, (void*)((intptr_t) i));
 	}
+	
 	for (int j = 0; j < numProducer; j++)
 	{
 		pthread_create(&producerThread[j], NULL, createProducer, 0);
 	}
 
 	/* wait for the thread to exit */
-	for (int i = 0; i < numConsumer; i++)
+	for (int i = 0; i < numProducer; i++)
 	{
-		pthread_join(consumerThread[i], NULL);
+		pthread_join(producerThread[i], NULL);
 	}
-	for (int j = 0; j < numProducer; j++)
+	
+	for (int j = 0; j < numConsumer; j++)
 	{
-		pthread_join(producerThread[j], NULL);
+		pthread_join(consumerThread[j], NULL);
 	}
 
 	// TODO These aren't in the right place.
@@ -83,7 +92,7 @@ void *createProducer(void *param) {
 		sem_wait(&empty);
 		pthread_mutex_lock(&bufferMutex);
 		
-		srand(pthread_self() * log(seed));
+		srand(pthread_self() * log(seed)); //log op on seed will generate new random numbers without having it become too large over time
 		seed++;
 		item = rand();
 		//int id = *((int*)(&param));
@@ -92,6 +101,7 @@ void *createProducer(void *param) {
 		if (insert_item(item) == 0)
 		{
 			cout << "Insertion Successful!" << endl;
+			cout << "_____________________" << endl;
 		}
 		else
 		{
@@ -100,7 +110,7 @@ void *createProducer(void *param) {
 		
 		pthread_mutex_unlock(&bufferMutex);
 		sem_post(&full);
-	} while(seed < BUFFER_SIZE);
+	} while(seed < BUFFER_SIZE * 2);
 
 	/*for (int i = 0; i < BUFFER_SIZE; i++)
 	{
@@ -142,6 +152,46 @@ void *createProducer(void *param) {
 }
 
 void *createConsumer(void *param) {
+	do 
+	{
+		//printStuff(pthread_self());
+		sem_wait(&full);
+		pthread_mutex_lock(&bufferMutex);
+		
+		//srand(pthread_self() * log(seed));
+		//seed++;
+		//item = rand();
+		//int id = *((int*)(&param));
+		//cout << "Thread: " << id <<" Is Inserting " << item << endl;
+		if (buffer[index] != 0)
+		{
+			cout << "Removing an item from buffer: " << buffer[index] << endl;
+			if (remove_item(element) == 0)
+			{
+				cout << "Removal Successful!" << endl;
+				cout << "_____________________" << endl;
+			}
+			else
+			{
+				cout << "Removal Unsuccessful." << endl;
+			}
+		}
+		else
+		{
+			cout << "Element at " << index << " is empty." << endl;
+			cout << "_____________________" << endl;
+		}
+		index = (index + 1) % BUFFER_SIZE;
+		element = &(buffer[index]);
+		
+		pthread_mutex_unlock(&bufferMutex);
+		sem_post(&empty);
+	} while(index < BUFFER_SIZE * 2 - 2);
+
+	/*for (int i = 0; i < BUFFER_SIZE; i++)
+	{
+		cout << buffer[i] << endl;
+	}*/
 //	do {
 //		sem_wait(&full);
 //		pthread_mutex_lock(&bufferMutex);
@@ -195,7 +245,18 @@ placing it in item.
 return 0 if successful, otherwise
 return -1 indicating an error condition.*/
 int remove_item(buffer_item *it) {
-	return 0;
+	int temp = *it;
+	for (int i = 0; i < BUFFER_SIZE; i++)
+	{
+		if (buffer[i] == temp)
+		{
+			//buffer[i] = 0;
+			//it = 0;
+			*it = 0;
+			return 0;
+		}	
+	}
+	return -1;
 }
 
 void printStuff(int tid)
