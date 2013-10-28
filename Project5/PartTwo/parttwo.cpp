b8153bad88b1ccf4380fbb1e29e1d2c85ba3f387
hue03 <gth626u@gmail.com>
2013-10-28 12:59:10 -0700

    Base source files for project 5.

---
+++ Project5/PartTwo/parttwo.cpp
@@ -0,0 +1,17 @@
+// CS3243 Operating Systems
+// Fall 2013
+// Project 4: Process Synchronization, Part 2
+// Steven Ng and Hue Moua
+// Date: 10/28/2013
+// File: parttwo.cpp
+
+#define MAX_PROCESSES 60	// This will not ever change
+#define PROCESS_COUNT 60	// useful when debugging to limit # of procs
+#define MIN_BURST 100
+#define MAX_BURST 5000
+#define MIN_MEMORY_PER_PROC 4
+#define MAX_MEMORY_PER_PROC 160
+#define MAX_MEMORY 1040
+#define MAX_BLOCK_PROC_RATIO 0.85
+#define PRINT_INTERVAL 5000	// # of cpu quanta between memory map printouts
+#define MAX_QUANTA 50000	// # quanta to run before ending simulation
