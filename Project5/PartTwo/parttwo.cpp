0f1a20cdf937fe085340d81d718c3c674e6f31e2
Steven Ng <stevenng308@gmail.com>
2013-11-13 22:18:00 -0500

    Starting part 2

--- Project5/PartTwo/parttwo.cpp
+++ Project5/PartTwo/parttwo.cpp
@@ -5,17 +5,16 @@
 // Date: 10/28/2013
 // File: parttwo.cpp
 
-#define MAX_PROCESSES 60	// This will not ever change
-#define PROCESS_COUNT 60	// useful when debugging to limit # of procs
-#define MIN_BURST 100
-#define MAX_BURST 5000
-#define MIN_MEMORY_PER_PROC 4
-#define MAX_MEMORY_PER_PROC 160
-#define MAX_MEMORY 1040
-#define MAX_BLOCK_PROC_RATIO 0.85
-#define PRINT_INTERVAL 5000	// # of cpu quanta between memory map printouts
+#define MAX_PROCESSES 52	// This will not ever change
+#define PROCESS_COUNT 23	// useful when debugging to limit # of procs
+#define MIN_DEATH_INTERVAL 20
+#define MAX_DEATH_INTERVAL 300
+#define MAX_FRAMES 720
+#define MAX_PAGES
+#define SHIFT_INTERVAL 10
+#define PRINT_INTERVAL 500	// # of cpu quanta between memory map printouts
 #define MAX_QUANTA 50000	// # quanta to run before ending simulation
-#define ENABLE_COMPACTION 0
+#define SLEEP_LENGTH
 
 int main()
 {
